635a2382839e444bd8c35a94dceac0b69d9b3626
huandmse171114 <huandmse171114@fpt.edu.vn>
2024-04-20 13:31:22 +0700

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # to download local package in virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name = 'mcqgenerator',
+    version = '0.0.1',
+    author = 'huandm',
+    author_email = 'huandmse171114@fpt.edu.vn',
+    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages = find_packages()
+)
